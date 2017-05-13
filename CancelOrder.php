efde9a628371129cdf25be68bbc778d16febc5be
Sirom028 <s5830213028@phuket.psu.ac.th>
2017-05-13 12:34:31 +0700

    Delete CancelOrder.php

--- CancelOrder.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $mid=$_POST['mid'];
-  $connect = mysqli_connect("localhost","id1494919_admin","02806","id1494919_movie");
-  $sql = 'Delete from list where  OID = "'.$mid.'"';
-  $result = mysqli_query($connect,$sql);
-  header("location:OrderLoginCompelete.php");
-  mysqli_close($connect);
-?>
